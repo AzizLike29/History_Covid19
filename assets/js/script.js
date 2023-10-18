b1a4d8868b43fdc7602cb3fe7a12312bfeb786e8
AzizLike29 <abdulfirdaus590@gmail.com>
2023-10-18 17:06:06 +0700

    perubahan js dan style

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,26 +16,45 @@ async function fetchDataByCountry(country) {
     const data = await response.json();
 
     if (response.ok) {
-      const statistics = data.response[0];
-      const country = statistics.country;
-      const continent = statistics.continent;
-      const day = statistics.day;
-      const activeCases = statistics.cases.active ?? "Null";
-      const totalCases = statistics.cases.total ?? "Null";
-      const criticalCases = statistics.cases.critical ?? "Null";
-      const totalDeaths = statistics.deaths.total ?? "Null";
-      const recoveredCases = statistics.cases.recovered ?? "Null";
-      const totalTests = statistics.tests.total ?? "Null";
+      if (data.results === 0) {
+        const errorAlert = document.getElementById("error");
+        errorAlert.innerHTML =
+          '<i class="fa-solid fa-circle-exclamation fa-beat"></i> Sorry, your data was not found';
+        errorAlert.classList.add("fade-in");
+        errorAlert.style.display = "block";
 
-      document.getElementById("country").textContent = country;
-      document.getElementById("continent").textContent = continent;
-      document.getElementById("day").textContent = day;
-      document.getElementById("active").textContent = activeCases;
-      document.getElementById("cases").textContent = totalCases;
-      document.getElementById("critical").textContent = criticalCases;
-      document.getElementById("deaths").textContent = totalDeaths;
-      document.getElementById("recovered").textContent = recoveredCases;
-      document.getElementById("tests").textContent = totalTests;
+        // Time to fade
+        setTimeout(() => {
+          errorAlert.classList.add("active");
+        }, 100);
+      } else {
+        const statistics = data.response[0];
+        const country = statistics.country;
+        const continent = statistics.continent;
+        const day = formatDate(statistics.day);
+        const activeCases = statistics.cases.active.toLocaleString() ?? "Null";
+        const totalCases = statistics.cases.total.toLocaleString() ?? "Null";
+        const criticalCases =
+          statistics.cases.critical.toLocaleString() ?? "Null";
+        const totalDeaths = statistics.deaths.total.toLocaleString() ?? "Null";
+        const recoveredCases =
+          statistics.cases.recovered.toLocaleString() ?? "Null";
+        const totalTests = statistics.tests.total.toLocaleString() ?? "Null";
+
+        document.getElementById("country").textContent = country;
+        document.getElementById("continent").textContent = continent;
+        document.getElementById("day").textContent = day;
+        document.getElementById("active").textContent = activeCases;
+        document.getElementById("cases").textContent = totalCases;
+        document.getElementById("critical").textContent = criticalCases;
+        document.getElementById("deaths").textContent = totalDeaths;
+        document.getElementById("recovered").textContent = recoveredCases;
+        document.getElementById("tests").textContent = totalTests;
+
+        const errorAlert = document.getElementById("error");
+        errorAlert.textContent = "";
+        errorAlert.style.display = "none";
+      }
     } else {
       console.error("Error fetching data:", data);
     }
@@ -48,6 +67,14 @@ function handleSubmitSearch(event) {
   event.preventDefault();
   const country = document.getElementById("countryInput").value;
   document.getElementById("countryInput").value = "";
+  fetchDataByCountry(country);
+}
+
+// validation date
+function formatDate(dateString) {
+  const options = { day: "2-digit", month: "short", year: "numeric" };
+  const today = new Date(dateString).toLocaleDateString(undefined, options);
+  return today;
 }
 
 // your form
