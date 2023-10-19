4ce24a5a00ba0b2ba71d8fea055c00292355a1e4
AzizLike29 <abdulfirdaus590@gmail.com>
2023-10-19 18:56:00 +0700

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const baseUrl = "https://covid-193.p.rapidapi.com/statistics";
 
 async function fetchDataByCountry(country) {
-  const url = `${baseUrl}?country=${country}`;
+  // Clear the data elements
+  clearData();
 
+  const url = `${baseUrl}?country=${country}`;
   const options = {
     method: "GET",
     headers: {
@@ -17,45 +19,11 @@ async function fetchDataByCountry(country) {
 
     if (response.ok) {
       if (data.results === 0) {
-        const errorAlert = document.getElementById("error");
-        errorAlert.innerHTML =
-          '<i class="fa-solid fa-circle-exclamation fa-beat"></i> Sorry, your data was not found';
-        errorAlert.classList.add("fade-in");
-        errorAlert.style.display = "block";
-
-        // Time to fade
-        setTimeout(() => {
-          errorAlert.classList.add("active");
-        }, 100);
+        displayError("Sorry, your data was not found");
       } else {
         const statistics = data.response[0];
-        const country = statistics.country;
-        const continent = statistics.continent;
-        const day = formatDate(statistics.day);
-        const activeCases =
-          statistics.cases.active?.toLocaleString() ?? "-/N/A";
-        const totalCases = statistics.cases.total?.toLocaleString() ?? "-/N/A";
-        const criticalCases =
-          statistics.cases.critical?.toLocaleString() ?? "-/N/A";
-        const totalDeaths =
-          statistics.deaths.total?.toLocaleString() ?? "-/N/A";
-        const recoveredCases =
-          statistics.cases.recovered?.toLocaleString() ?? "-/N/A";
-        const totalTests = statistics.tests.total?.toLocaleString() ?? "-/N/A";
-
-        document.getElementById("country").textContent = country;
-        document.getElementById("continent").textContent = continent;
-        document.getElementById("day").textContent = day;
-        document.getElementById("active").textContent = activeCases;
-        document.getElementById("cases").textContent = totalCases;
-        document.getElementById("critical").textContent = criticalCases;
-        document.getElementById("deaths").textContent = totalDeaths;
-        document.getElementById("recovered").textContent = recoveredCases;
-        document.getElementById("tests").textContent = totalTests;
-
-        const errorAlert = document.getElementById("error");
-        errorAlert.textContent = "";
-        errorAlert.style.display = "none";
+        updateDataElements(statistics);
+        clearError();
       }
     } else {
       console.error("Error fetching data:", data);
@@ -63,6 +31,57 @@ async function fetchDataByCountry(country) {
   } catch (error) {
     console.error("Fetch error:", error);
   }
+}
+
+function clearData() {
+  // Clear the data elements
+  document.getElementById("country").textContent = "";
+  document.getElementById("continent").textContent = "";
+  document.getElementById("day").textContent = "";
+  document.getElementById("active").textContent = "";
+  document.getElementById("cases").textContent = "";
+  document.getElementById("critical").textContent = "";
+  document.getElementById("deaths").textContent = "";
+  document.getElementById("recovered").textContent = "";
+  document.getElementById("tests").textContent = "";
+}
+
+function displayError(message) {
+  const errorAlert = document.getElementById("error");
+  errorAlert.innerHTML =
+    '<i class="fa-solid fa-circle-exclamation fa-beat"></i> ' + message;
+  errorAlert.classList.add("fade-in");
+  errorAlert.style.display = "block";
+
+  // Time to fade
+  setTimeout(() => {
+    errorAlert.classList.add("active");
+  }, 100);
+}
+
+function clearError() {
+  const errorAlert = document.getElementById("error");
+  errorAlert.textContent = "";
+  errorAlert.style.display = "none";
+}
+
+function updateDataElements(statistics) {
+  // Update the data elements
+  document.getElementById("country").textContent = statistics.country;
+  document.getElementById("continent").textContent = statistics.continent;
+  document.getElementById("day").textContent = formatDate(statistics.day);
+  document.getElementById("active").textContent =
+    statistics.cases.active?.toLocaleString() ?? "-/N/A";
+  document.getElementById("cases").textContent =
+    statistics.cases.total?.toLocaleString() ?? "-/N/A";
+  document.getElementById("critical").textContent =
+    statistics.cases.critical?.toLocaleString() ?? "-/N/A";
+  document.getElementById("deaths").textContent =
+    statistics.deaths.total?.toLocaleString() ?? "-/N/A";
+  document.getElementById("recovered").textContent =
+    statistics.cases.recovered?.toLocaleString() ?? "-/N/A";
+  document.getElementById("tests").textContent =
+    statistics.tests.total?.toLocaleString() ?? "-/N/A";
 }
 
 function handleSubmitSearch(event) {
